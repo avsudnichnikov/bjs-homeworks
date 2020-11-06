7dc1844dc9eca23f3d95ef63133d14322f3b4f59
avsudnichnikov <avsudnichnikov@gmail.com>
2020-11-06 22:46:09 +0500

    add solution of task#1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,37 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+    if (isNaN(Number(percent))) {
+        return 'Неверно указан параметр "Процентная ставка"';
+    }
+    if (isNaN(Number(contribution))) {
+        return 'Неверно указан параметр "Первоначальный взнос"';
+    }
+    if (isNaN(Number(amount))) {
+        return 'Неверно указан параметр "Общая стоимость"';
+    }
+    if (new Date(date).getFullYear() < new Date().getFullYear()) {
+        return 'Неверно указан параметр "Дата окончания"';
+    }
+
+    const deltaMonth = new Date(date).getMonth() - new Date().getMonth();
+    const deltaYear = new Date(date).getFullYear() - new Date().getFullYear();
+
+    const paymentPeriod = 12 * deltaYear + deltaMonth;
+    const creditBody = amount - contribution;
+    const monthlyGrowth = percent / 1200;
+    const monthlyPayment = creditBody * monthlyGrowth * (1 + 1 / (((1 + monthlyGrowth) ** paymentPeriod) - 1));
+
+    const totalPayment = Number((monthlyPayment * paymentPeriod).toFixed(2));
+
+    console.log(totalPayment);
+
+    return totalPayment;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    const greeting = `Привет, мир! Меня зовут ${name || 'Аноним'}.`
+
+    console.log(greeting);
+
+    return greeting;
 }

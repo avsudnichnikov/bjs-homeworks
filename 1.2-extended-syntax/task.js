1db0ec8bd9cc663f9cc86a4076f85d42470a3c3b
avsudnichnikov <avsudnichnikov@gmail.com>
2020-11-06 21:37:18 +0500

    add solution of task#1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,39 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c) {
+    "use strict";
+    const result = [];
+
+    const d = b ** 2 - 4 * a * c;
+
+    if (d >= 0) {
+        result.push((-b + Math.sqrt(d)) / 2 / a);
+    }
+    if (d > 0) {
+        result.push((-b - Math.sqrt(d)) / 2 / a);
+    }
+
+    return result;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+    let sum = 0;
+
+    if (marks.length >= 5) {
+        marks = marks.slice(0, 5);
+    }
+
+    for (let i = 0; i < marks.length; i += 1) {
+        sum = sum + marks[i];
+    }
+
+    return sum / marks.length || 0;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+    let result;
+    if ((new Date().getFullYear() - dateOfBirthday.getFullYear()) < 18) {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    } else {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    }
+    return result;
 }
